d66125d9ffa0449d1814e7e2f9ad53553258261b
githubfoam <lequestionis@gmail.com>
2020-06-16 16:24:16 +0300

    He died fast, and if we have any choice about it, that's the way we all should go out.

--- deploy-openesb.sh
+++ deploy-openesb.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-kubectl apply -f kubectl apply -f https://openebs.github.io/charts/openebs-operator.yaml #install OpenEBS
+# openesb component list
+#https://github.com/openebs/openebs/blob/master/k8s/openebs-operator.yaml
+
+kubectl apply -f https://openebs.github.io/charts/openebs-operator.yaml #install OpenEBS
 kubectl get service --all-namespaces # find a Service IP,list all services in all namespaces-
 kubectl get pods -n openebs -l openebs.io/component-name=openebs-localpv-provisioner #Observe localhost provisioner pod
 kubectl get sc #Check the storage Class
-echo "Waiting for openebs-localpv-provisioner to be ready ..."
+echo echo "Waiting for openebs-localpv-provisioner component to be ready ..."
 for i in {1..60}; do # Timeout after 5 minutes, 150x5=300 secs
       if sudo kubectl get pods --namespace=openebs -l openebs.io/component-name=openebs-localpv-provisioner | grep Running ; then
+        break
+      fi
+      sleep 5
+done
+echo echo "Waiting for maya-apiserver component to be ready ..."
+for i in {1..60}; do # Timeout after 5 minutes, 150x5=300 secs
+      if sudo kubectl get pods --namespace=openebs -l openebs.io/component-name=maya-apiserver | grep Running ; then
         break
       fi
       sleep 5
