54e9b6dacc6bdd8b8c1c3633a18d36eb85a143b7
githubfoam <lequestionis@gmail.com>
2020-06-23 01:41:07 +0300

    ещё на э́той неде́ле

--- deploy-multipass-microk8s.sh
+++ deploy-multipass-microk8s.sh
@@ -2,8 +2,8 @@
 
 echo "=============================Multipass============================================================="
 multipass launch --name node1 #must be named as node1
-multipass exec node1 -sudo sh -c "curl -sfL https://get.k3s.io | sh -"
-TOKEN=`multipass exec node1 -sudo cat /var/lib/rancher/k3s/server/node-token`
+multipass exec node1 -- sudo sh -c "curl -sfL https://get.k3s.io | sh -"
+TOKEN=`multipass exec node1 -- sudo cat /var/lib/rancher/k3s/server/node-token`
 echo $TOKEN
 IP=`multipass list --format csv | grep "^node1" | cut -d, -f3`
 echo $IP
@@ -11,10 +11,10 @@ COUNT=3 &&  echo $COUNT
 for i in `seq 2 $COUNT`
   do
       multipass launch --name node$i | tee /dev/null
-      multipass exec node$i -sudo sh -c "curl -sfL https://get.k3s.io | K3S_URL=https://$IP:6443 K3S_TOKEN=$TOKEN sh -"
+      multipass exec node$i -- sudo sh -c "curl -sfL https://get.k3s.io | K3S_URL=https://$IP:6443 K3S_TOKEN=$TOKEN sh -"
 done
 sleep 10
-multipass exec node1 sudo kubectl get nodes
+multipass exec node1 -- sudo kubectl get nodes
 multipass mount $HOME node1
 multipass mount $HOME node1:/tmp
 echo $HOME && ls -lai
@@ -33,44 +33,8 @@ multipass info kubeflow # if you used Multipass, you can get the IP address of t
 multipass list # if you used Multipass, you can get the IP address of the VM with either
 # http://” Your kubeflow VM IP”:“Ambassador PORT”
 
-# multipass exec node1 -- sudo sh -c "kubectl get pods --all-namespaces" # verify Traefik installed by default
-# multipass exec node1 -- sudo sh -c "kubectl get pods -n kube-system" # verify Traefik installed by default
-# multipass exec node1 -- sudo sh -c "kubectl get nodes" # verify Traefik installed by default
-
-# echo "=============================openEBS============================================================="
-# echo $HOME && ls -lai
-# multipass transfer deploy-openesb.sh node1:deploy-openesb.sh
-# multipass exec node1 -sudo mkdir app
-# multipass transfer app/deploy-openesb.sh node1:app/deploy-openesb.sh
-# multipass transfer app/deploy-openesb.sh node1:deploy-openesb.sh
-# multipass transfer Makefile node1:Makefile
-# multipass exec node1 -sudo ls -lai
-# multipass exec node1 -sudo apt-get install -qqy build-essential #required 4 Makefile
-# # multipass exec node1 -sudo bash deploy-openesb.sh
-# multipass exec node1 -sudo ls -lai app
-# multipass exec node1 -sudo ls -lai app/deploy-openesb.sh #app/deploy-openesb.sh: No such file or directory
-# multipass exec node1 -sudo make deploy-openesb
+# Getting started with Jupyter notebooks on Kubeflow
+# https://www.kubeflow.org/docs/notebooks/setup/
 
 
-
-
-# multipass exec node1 -sudo sh -c "./deploy-openesb.sh"
-# multipass exec node1 -sudo sh -c "kubectl apply -f https://openebs.github.io/charts/openebs-operator.yaml"
-# echo "=============================Kubernetes Dashboard============================================================="
-#Deploying the Kubernetes Dashboard
-#Access the cluster from PC remotely
-# cat /etc/rancher/k3s/k3s.yaml
-# Save the copied configuration as ~/.kube/config
-# edit the file and change the line server: https://localhost:6443 server: https://k3smaster:6443
-# chmod 600 ~/.kube/config   limit the file's read/write permissions
-# mkdir ~/.kube #Make a directory for the config Copy this configuration information
-# apt update && sudo apt install -y apt-transport-https
-# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-# echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | \
-# tee -a /etc/apt/sources.list.d/kubernetes.list
-# apt update && apt install kubectl
-# kubectl get nodes check out  cluster from PC
-#Access the cluster from PC remotely
-
-
-# multipass purge #purge to remove all deleted instances completely
+multipass purge #purge to remove all deleted instances completely
